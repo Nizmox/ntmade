605234e076bec27e8e6465a06976ad8bf571fdf6
Nizmox <nizmox@gmail.com>
2013-11-26 19:59:48 +1100

    added articles preview to code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :get_footer_content
+
+  protected
+  def get_footer_content
+    @footer = Content.where("name = 'Footer'").first.body 
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
 
   def index
     @body = Content.where("name = 'About'").first.body
+
+    @articles = Article.all #.order(:created_at)
   end
 
 end
